4516c1d23d740313d29975a1c8bb0aa2813788d6
Василий Веркин <v.verkin@gmail.com>
2023-09-01 23:17:41 +0300

    решение задач

--- src/js/modul04-lesson11/rectangle.js
+++ src/js/modul04-lesson11/rectangle.js
@@ -12,3 +12,35 @@
 
 По умолчанию значения высоты и ширины заданы 5 см
 */
+
+const rectangle = {
+
+  _width: 5,
+  _height: 5,
+
+  get perimeter() {
+    return `${(this._width + this._height) * 2} см`;
+  },
+
+  get square() {
+    return `${this._width * this._height} см2`;
+  },
+
+  set width(newWidth) {
+    if (typeof newWidth === 'number') {
+      return this._width;
+    }
+  },
+
+  set height(newHeight) {
+    if (typeof newHeight === 'number') {
+      return this._height;
+    }
+  },
+};
+
+rectangle.newWidth = 100;
+rectangle.newHeight = '456';
+
+console.log(rectangle.perimeter);
+console.log(rectangle.square);
