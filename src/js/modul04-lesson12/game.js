a8298235f830494ab49e94ca05ad1960d70b746d
Vasilii Verkin <v.verkin@gmail.com>
2023-09-19 21:28:08 +0300

    набросал решение задачи

--- src/js/modul04-lesson12/game.js
+++ src/js/modul04-lesson12/game.js
@@ -3,11 +3,17 @@
 (() => {
   const figures = ['камень', 'ножницы', 'бумага'];
 
-  const getRandomIntInclusive = (min, max) => {
+  const getRandomIntInclusive = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
-  };
+  const computerChoice = getRandomIntInclusive(0, 2);
 
   const game = () => {
-
+    const result = {
+      player: 0,
+      computer: 0,
+    };
+    return function start() {
+      const userChoice = prompt('Выбери камень, ножницы или бумагу', '');
+    };
   };
 })();
