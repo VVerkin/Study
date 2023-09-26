ce0f6802137e699fdaca3c97793f82727bcb47c8
Vasilii Verkin <v.verkin@gmail.com>
2023-09-26 22:19:54 +0300

    игра камень, ножницы, бумага

--- src/js/modul04-lesson12/game.js
+++ src/js/modul04-lesson12/game.js
@@ -3,28 +3,29 @@
 (() => {
   const figures = ['камень', 'ножницы', 'бумага'];
 
-  const getRandomIntInclusive = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
-  const computerNumber = getRandomIntInclusive(0, 2);
-  console.log(`Случайное число: ${computerNumber}`);
-  const computerChoice = figures[computerNumber];
-  console.log(`Выбор компьютера: ${computerChoice}`);
-
   const game = () => {
     const result = {
       player: 0,
       computer: 0,
     };
     return function start() {
+      const getRandomIntInclusive = (min, max) => {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1) + min);
+      };
+      const computerNumber = getRandomIntInclusive(0, 2);
+      console.log(`Случайное число: ${computerNumber}`);
+      const computerChoice = figures[computerNumber];
+      console.log(`Выбор компьютера: ${computerChoice}`);
+
       const getUserNumber = () => {
         const userChoice = prompt(`Выбери: ${figures.join(', ')}`, '');
         if (userChoice === null) {
           console.log(`Выбор пользователя: ${userChoice}`);
           return;
-        } else if (!figures.includes(userChoice.toLowerCase())) {
+        }
+        if (!figures.includes(userChoice.toLowerCase())) {
           alert('Можно выбирать только предложенные варианты');
           return getUserNumber();
         }
@@ -36,21 +37,23 @@
       const getWinner = () => {
         const subtraction = computerNumber - userNumber;
         console.log(`разница: ${subtraction}`);
-        if (subtraction === 1 || subtraction === -2) {
+
+        if (subtraction === 0) {
+          alert('Ничья');
+          return;
+        } else if (subtraction === 1 || subtraction === -2) {
           alert(`Ты победил! Компьютер выбрал ${computerChoice}`);
           result.player += 1;
+          return;
         } else if (subtraction === -1 || subtraction === 2) {
           alert(`Ты проиграл! Компьютер выбрал ${computerChoice}`);
           result.computer += 1;
-        } else {
-          alert('Ничья');
+          return;
         }
         console.log(result);
       };
       getWinner();
-      return start();
     };
-
   };
   window.RPS = game;
 })();
