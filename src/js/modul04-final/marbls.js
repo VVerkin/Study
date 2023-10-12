b0b9d60262b969bd230e47d94621ecd6bccfb8be
Vasilii Verkin <v.verkin@gmail.com>
2023-10-12 23:38:44 +0300

    игра Марблы

--- src/js/modul04-final/marbls.js
+++ src/js/modul04-final/marbls.js
@@ -1,66 +1,73 @@
 'use strict';
 
 (() => {
-  const figures = ['камень', 'ножницы', 'бумага'];
+  // const figures = ['камень', 'ножницы', 'бумага'];
 
   const game = () => {
-    const ballsNumber = {
+    const balls = {
       player: 5,
       bot: 5,
     };
+    console.log(balls);
     return function start() {
-      // const getRandomInt = (min, max) => {
-      //   min = Math.ceil(min);
-      //   max = Math.floor(max);
-      //   return Math.floor(Math.random() * (max - min + 1) + min);
-      // };
-      // const computerNumber = getRandomInt(0, 2);
-      // console.log(`Случайное число: ${computerNumber}`);
-      // const computerChoice = figures[computerNumber];
-      // console.log(`Выбор компьютера: ${computerChoice}`);
+      const getRandomInt = (min, max) => {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1) + min);
+      };
+      const computerNumber = getRandomInt(1, `${balls.player}`);
+      console.log(`Случайное число: ${computerNumber}`);
+      if (computerNumber % 2 === 0) {
+        console.log(`четное`);
+      } else {
+        console.log(`не четное`);
+      }
 
       const getUserNumber = () => {
-        let userNumber = prompt(`Введи число от 1 до  ${ballsNumber.player}`, '');
+        const userNumber = prompt(`Введи число от 1 до  ${balls.player}`, '');
         if (userNumber === null) {
           const endGame = confirm('Точно хочешь выйти?');
           if (endGame === false) {
             return getUserNumber();
           }
-          alert(`Счет: игрок - ${ballsNumber.player}, бот - ${ballsNumber.bot}`);
+          alert(`Счет: игрок - ${balls.player}, бот - ${balls.bot}`);
           return;
-        } else if (userNumber < 1 || userNumber > ballsNumber.player) {
-          alert(`Число должно быть от 1 до ${ballsNumber.player}`);
+        } else if (userNumber < 1 || userNumber > balls.player) {
+          alert(`Число должно быть от 1 до ${balls.player}`);
           return getUserNumber();
         }
         if (isNaN(userNumber)) {
-          userNumber = +userNumber;
-        } else {
           alert('Введи число');
+        } else {
+          return +userNumber;
         }
         return getUserNumber();
       };
-      console.log(getUserNumber());
+      const playerNumber = getUserNumber();
+      console.log(playerNumber);
 
-      // const getWinner = () => {
-      //   const subtraction = computerNumber - userNumber;
-      //   console.log(`разница: ${subtraction}`);
+      const getWinner = () => {
+      if (balls.bot > 0 || balls.player > 0) {
 
-      //   if (subtraction === 0) {
-      //     alert('Ничья');
-      //     return start();
-      //   } else if (subtraction === 1 || subtraction === -2) {
-      //     alert(`Ты победил! Компьютер выбрал ${computerChoice}`);
-      //     result.player += 1;
-      //     return start();
-      //   } else if (subtraction === -1 || subtraction === 2) {
-      //     alert(`Ты проиграл! Компьютер выбрал ${computerChoice}`);
-      //     result.computer += 1;
-      //     return start();
-      //   }
-      //   console.log(result);
-      // };
-      // getWinner();
+          if ((playerNumber % 2 === 0 && computerNumber % 2 === 0) || (playerNumber % 2 !== 0 && computerNumber % 2 !== 0)) {
+          alert('Компьютер угадал!');
+            balls.bot += playerNumber;
+            balls.player -= playerNumber;
+        } else {
+          alert('Компьютер не угадал!');
+          balls.player += playerNumber;
+          balls.bot -= playerNumber;
+        }
+      } else {
+        alert('игра закончена!');
+      }
+      return start();
+    };
+      console.log(balls);
+      getWinner();
     };
   };
   window.marbls = game;
 })();
+
+
