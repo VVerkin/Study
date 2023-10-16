bc52c3f3b1d51a3767842c79a1611ae5901b938f
Vasilii Verkin <v.verkin@gmail.com>
2023-10-16 23:05:58 +0300

    игра марблы

--- src/js/modul04-final/marbls.js
+++ src/js/modul04-final/marbls.js
@@ -1,67 +1,66 @@
 'use strict';
 
 (() => {
-  // const figures = ['камень', 'ножницы', 'бумага'];
+  const evenOdd = ['четное', 'нечетное'];
 
   const game = () => {
     const balls = {
       player: 5,
       bot: 5,
     };
-    console.log(balls);
     return function start() {
       const getRandomInt = (min, max) => {
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min + 1) + min);
       };
-      const computerNumber = getRandomInt(1, `${balls.player}`);
-      console.log(`Случайное число: ${computerNumber}`);
-      if (computerNumber % 2 === 0) {
-        console.log(`четное`);
-      } else {
-        console.log(`не четное`);
-      }
+      const computerChoice = getRandomInt(0, evenOdd.length - 1);
+      console.log(`Выбор компьютера: ${computerChoice}`);
+
 
       const getUserNumber = () => {
-        const userNumber = prompt(`Введи число от 1 до  ${balls.player}`, '');
+        let userNumber = prompt(`Введи число от 1 до  ${balls.player}`, '');
         if (userNumber === null) {
-          const endGame = confirm('Точно хочешь выйти?');
-          if (endGame === false) {
-            return getUserNumber();
-          }
+          const quitGame = confirm('Точно хочешь выйти?');
+          if (quitGame === false) return getUserNumber();
           alert(`Счет: игрок - ${balls.player}, бот - ${balls.bot}`);
           return;
-        } else if (userNumber < 1 || userNumber > balls.player) {
+        }
+
+        if (userNumber < 1 || userNumber > balls.player) {
           alert(`Число должно быть от 1 до ${balls.player}`);
           return getUserNumber();
         }
-        if (isNaN(userNumber)) {
-          alert('Введи число');
+
+        if (!isNaN(userNumber)) {
+          userNumber = +userNumber;
+
+          if (userNumber > 1 || userNumber < balls.player) return userNumber;
+          alert(`Число должно быть от 1 до ${balls.player}`);
         } else {
-          return +userNumber;
+          alert('Введи число');
         }
         return getUserNumber();
       };
-      const playerNumber = getUserNumber();
-      console.log(playerNumber);
+      const userChoice = getUserNumber();
+      console.log(userChoice);
 
       const getWinner = () => {
-        if (balls.bot > 0 && balls.player > 0) {
-          if ((playerNumber % 2 === 0 && computerNumber % 2 === 0) ||
-          (playerNumber % 2 !== 0 && computerNumber % 2 !== 0)) {
+        if (balls.bot < 1 || balls.player < 1) {
+          alert('игра окончена!!!!');
+          return;
+        } else if ((userChoice % 2 === 0 && computerChoice === 0) ||
+          (userChoice % 2 !== 0 && computerChoice === 1)) {
             alert('Компьютер угадал!');
-            balls.bot += playerNumber;
-            balls.player -= playerNumber;
+            balls.bot += userChoice;
+            balls.player -= userChoice;
+            return start();
           } else {
             alert('Компьютер не угадал!');
-            balls.player += playerNumber;
-            balls.bot -= playerNumber;
+            balls.player += userChoice;
+            balls.bot -= userChoice;
+            return start();
           }
-        } else {
-          alert('игра закончена!');
-        }
-        return start();
       };
       console.log(balls);
       getWinner();
