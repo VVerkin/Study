012d3eb70acc2a247b85cfc0d5bdffc653ba7a2b
Vasilii Verkin <v.verkin@gmail.com>
2024-01-15 23:59:00 +0300

    подготовил модули

---
+++ src/js/modul05-lesson09/lesson5.9.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const $ = (() => {
+  const privatVar = 'Приватные данные';
+  const openVar = 'Открытые данные';
+  const app = () => {
+    console.log('Моё новое крутое приложение');
+  };
+
+  const getData = () => privatVar;
+
+  return {
+    app,
+    openVar,
+    getData
+  };
+})();
+
+console.log($);
+
+$.app();
+console.log($.openVar);
+console.log($.getData());
