b3fd570f4bed74084649a92943119f5b87995bc9
Vasilii Verkin <v.verkin@gmail.com>
2024-05-21 21:16:41 +0300

    реализован таймер

--- src/js/modul06-lesson2/timer.js
+++ src/js/modul06-lesson2/timer.js
@@ -1,23 +1,22 @@
 const timer = deadline => {
-    const timerBlockHour = document.querySelector('.tiner__block_hour');
-    const timerBlockMin = document.querySelector('.tiner__block_min');
-    const timerBlockSec = document.querySelector('.tiner__block_sec');
-  
-    const getTimeRemaining = () => {
-      // Дата, когда функция должна завершить свое выполнение и сразу получаем от нее таймстемп
-      const dateStop = new Date(deadline).getTime();
-      // Получаем текущую дату
-      const dateNow = Date.now();
-      console.log(dateNow);
-      // Ролучаем время, по истечение которгого таймер остановится
-      const timeRemaining = dateStop - dateNow;
-  
+  const timerBlockHour = document.querySelector('.timer__block_hour');
+  const timerBlockMin = document.querySelector('.timer__block_min');
+  const timerBlockSec = document.querySelector('.timer__block_sec');
+
+  const getTimeRemaining = () => {
+    // Дата, когда функция должна завершить свое выполнение и сразу получаем от нее таймстемп
+    const dateStop = new Date(deadline).getTime();
+    // Получаем текущую дату
+    const dateNow = Date.now();
+    // Ролучаем время, по истечение которгого таймер остановится
+    const timeRemaining = dateStop - dateNow;
+
     const seconds = (Math.floor(timeRemaining / 1000 % 60)); // В секундах
     const minutes = (Math.floor(timeRemaining / 1000 / 60 % 60)); // В минутах
     const hours = (Math.floor(timeRemaining / 1000 / 60 / 60)); // В часах
 
     return {timeRemaining, seconds, minutes, hours};
-    };
+  };
 
   const start = () => {
     const timer = getTimeRemaining();
@@ -25,9 +24,18 @@ const timer = deadline => {
     timerBlockHour.textContent = timer.hours;
     timerBlockMin.textContent = timer.minutes;
     timerBlockSec.textContent = timer.seconds;
+
+    const intervalId = setTimeout(start, 1000);
+
+    if (timer.timeRemaining <= 0) {
+      clearTimeout(intervalId);
+      timerBlockHour.textContent = '00';
+      timerBlockMin.textContent = '00';
+      timerBlockSec.textContent = '00';
+    }
   };
 
   start();
 };
-  
-timer('2024/04/20 22:00');
+
+timer('2024/05/21 21:45');
